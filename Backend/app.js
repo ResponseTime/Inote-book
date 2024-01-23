c0cd7291a4eed26d54dec4db68499abb098bf038
Looper <aayush.bhattacharjee2002@gmail.com>
2024-01-23 11:54:44 +0530

    added api integration

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const connect = require("./db.js");
-
+const cors = require("cors");
 connect();
 app.use(express.json());
 app.use("api/auth", require("./routes/auth"));
 app.use("api/notes", require("./routes/notes"));
-
+app.use(cors())
 app.listen(port, () => {
   console.log("Running");
 });
--- frontend/src/context/notes/Notestate.js
+++ frontend/src/context/notes/Notestate.js
@@ -4,15 +4,51 @@ import NoteContext from "./noteContext";
 const NoteSate = (props) => {
     const notesInit = [{ "id": 2323, "title": "my title", "description": "my description" }, { "id": 2323, "title": "my title", "description": "my description" }, { "id": 25323, "title": "my title", "description": "my description" }, { "id": 234323, "title": "my title", "description": "my description" }]
     const [notes, setNotes] = useState(notesInit)
-    const add_ = (title, description, tag) => {
+    const get_ = async () => {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': ""
+            }
+        })
+        const json = response.json()
+        setNotes(json)
+    }
+    const add_ = async (title, description, tag) => {
         const note = { "title": title, "description": description, "tag": tag };
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': ""
+            },
+            body: JSON.stringify(note)
+        })
+        const json = response.json()
         setNotes(notes.concat(note))
     }
     const delete_ = (id) => {
         setNotes(notes.filter((note) => { return note.id !== id }))
     }
-    const edit_ = (id, title, description, tag) => {
-
+    const edit_ = async (id, title, description, tag) => {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': ""
+            },
+            body: JSON.stringify({ title, description, tag })
+        })
+        const json = response.json()
+        for (let ind = 0; ind < notes.length; ind++) {
+            const ele = notes[ind];
+            if (ele.id === id) {
+                ele.title = title;
+                ele.description = description;
+                ele.tag = tag;
+            }
+        }
     }
     return (
         <NoteContext.Provider value={{ notes, add_, delete_, edit_ }}>
